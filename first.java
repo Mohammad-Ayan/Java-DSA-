5d39f11c6b82b05fc861689dfff48cfa6668d302
Mohammad-Ayan <ayan2015786@gmail.com>
2023-08-11 15:38:49 +0530

    second

--- first.java
+++ first.java
@@ -11,20 +11,63 @@ public class first{
     }
     
 } */
- public static void pattern(int n) {
+//  public static void pattern(int n) {
  
-    for (int i = 0; i<n; i++){
-       for (int j = 0; j<i+1; j++){
-       System.out.print("*");
-       }
+//     for (int i = 0; i<n; i++){
+//        for (int j = 0; j<i+1; j++){
+//        System.out.print("*");
+//        }
 
-       System.out.println();
+//        System.out.println();
+//     }
+// }
+
+//        public static void main(String[] args) {
+//         int n = 5; // Change this to the desired number
+//         pattern (n);
+//        }
+//     }
+
+//Recursive_Function
+// static int sumRec(int n){
+//     if(n==1){
+//     return 1;
+// }
+// return n + sumRec(n-1);
+// }
+// public static void main(String[] args) {
+//     System.out.println(sumRec(5));
+// }
+// }
+
+// public static void pattern(int n) {
+ 
+//      for (int i = 0; i<n; i++){
+//         for (int j = n; j>i+1; j--){
+//         System.out.print("*");
+//         }
+
+//         System.out.println();
+//      }
+//  }
+
+//         public static void main(String[] args) {
+//          int n = 5; // Change this to the desired number
+//         pattern (n);
+//         }
+//      }
+
+
+     public static int fib(int n){
+        if (n==1 || n==2){
+        return n-1;
+     }
+     else{
+        return fib (n-1) + (n-2);}
     }
-}
 
-       public static void main(String[] args) {
-        int n = 5; // Change this to the desired number
-        pattern (n);
-       }
-    }
-
+     public static void main(String[] args) {
+        int result = fib(7);
+        System.out.println(result);
+     }
+     }

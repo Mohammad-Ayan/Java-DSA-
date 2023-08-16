028740e6f0e65f3b0fd64261e4207b83c27ebcdf
Mohammad-Ayan <ayan2015786@gmail.com>
2023-08-17 00:15:09 +0530

    Questions

---
+++ custom.java
@@ -0,0 +1,17 @@
+
+class Employee{
+int id;
+String name;
+}
+
+public class custom {
+    public static void main(String[] args) {
+       Employee aynye = new Employee();
+        System.out.println(It is our custom class);
+        aynye.id = 23;
+        aynye.name = "aynye";
+
+        System.out.println(aynye.id);
+    }
+    
+}
--- first.java
+++ first.java
@@ -1,16 +1,17 @@
+import java.util.Scanner;
 public class first{
 
-   /*  public static void main(String[] args) {
-        int number = 5; // Change this to the desired number
-        printMultiplicationTable(number);
-    }
- public static void printMultiplicationTable(int n) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(n + " x " + i + " = " + (n * i));
-        }
-    }
+//      public static void main(String[] args) {
+//         int number = 5; // Change this to the desired number
+//         printMultiplicationTable(number);
+//     }
+//  public static void printMultiplicationTable(int n) {
+//         for (int i = 1; i <= 10; i++) {
+//             System.out.println(n + " x " + i + " = " + (n * i));
+//         }
+//     }
     
-} */
+// } 
 //  public static void pattern(int n) {
  
 //     for (int i = 0; i<n; i++){
@@ -28,7 +29,7 @@ public class first{
 //        }
 //     }
 
-//Recursive_Function
+// Recursive_Function
 // static int sumRec(int n){
 //     if(n==1){
 //     return 1;
@@ -58,16 +59,74 @@ public class first{
 //      }
 
 
-     public static int fib(int n){
-        if (n==1 || n==2){
-        return n-1;
-     }
-     else{
-        return fib (n-1) + (n-2);}
-    }
+//      public static int fib(int n){
+//         if (n==1 || n==2){
+//         return n-1;
+//      }
+//      else{
+//         return fib (n-1) + (n-2);
+//     }
+//     }
 
-     public static void main(String[] args) {
-        int result = fib(7);
-        System.out.println(result);
-     }
-     }
+//      public static void main(String[] args) {
+//         int result = fib(7);
+//         System.out.println(result);
+//      }
+//      }
+
+// static void pattern_recc(int n) {
+//         if (n>0){
+//              pattern_recc(n-1);{
+ 
+//       for (int i = 0; i<n; i++){
+//       System.out.print("*");
+//       }
+//       System.out.println();
+//         }
+//     }
+// }
+
+// public static void main(String[] args) {
+//          int result = (7);
+//          pattern_recc(result);
+//       }
+//     }
+ 
+// public static void main(String[] args) {
+//     Scanner scanner = new Scanner(System.in);
+
+//     System.out.print("Temperature in Celsius: ");
+//     double celsius = scanner.nextDouble();
+
+//     double fahrenheit = (celsius * 9/5) + 32;
+
+//     System.out.println("Temperature in Fahrenheit: "+ fahrenheit);
+
+//     scanner.close();
+
+// }
+// }
+
+
+// public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+//     float celsius=sc.nextFloat();
+//     float fahrenheit=(celsius*9/5)+32;
+//     System.out.println(fahrenheit);
+// }
+// }
+
+
+// public static void main(String[] args) {
+//     Scanner scanner = new Scanner(System.in);
+
+//     System.out.print("Temperature in Celsius: ");
+//      double celsius = scanner.nextDouble();
+
+//     double fahrenheit = (celsius * 9/5) + 32;
+
+//      System.out.println("Temperature in Fahrenheit: "+ fahrenheit);
+
+    
+// }
+// }

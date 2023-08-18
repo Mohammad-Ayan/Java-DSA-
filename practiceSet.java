5faed5c096dc47375a1bd7449cd4645100aedcdf
Mohammad-Ayan <ayan2015786@gmail.com>
2023-08-18 22:15:01 +0530

    PracticeSet

---
+++ practiceSet.java
@@ -0,0 +1,120 @@
+
+
+    class Employee{
+
+        int salary;
+        String name;
+
+        public int getSalary(){
+            return salary;
+        }
+
+        public String getName(){
+            return name;
+        }
+        public void setName (String n){
+            name = n;
+        }
+        
+
+        
+    }
+
+    class CellPhone{
+        public void ring(){
+            System.out.println("ringing...");
+        }
+         public void vibrate(){
+            System.out.println("vibrating...");
+         }
+          public void callFriend(){
+            System.out.println("call Tom...");
+    }
+}
+
+class Square{
+    int side;
+public int area(){
+    return side*side;
+}
+public int perimeter(){
+    return 4*side;
+}
+}
+
+class Rectangle{
+    int side1;
+    int side2;
+    public int area(){
+        return side1*side2;
+    } 
+    public int perimeter(){
+        return 2*(side1+side2);
+    } 
+}
+
+class TommyVecetti{
+    public void hit(){
+        System.out.println("hitting");
+    }
+    public void run(){
+        System.out.println("running");
+    }
+    public void fire(){
+        System.out.println("firing");
+    }
+}
+
+class Circle{
+    int radius;
+    public int area(){
+        return 22/7*
+    }
+
+}
+
+
+public class practiceSet {
+
+public static void main(String[] args) {
+/*
+// Ques 1
+    Employee jerry = new Employee();
+    jerry.salary = 345;
+    jerry.setName("CodeWithMe");
+    System.out.println(jerry.getName());
+    System.out.println(jerry.getSalary());
+
+ // Ques 2   
+    CellPhone vivo = new CellPhone();
+    vivo.callFriend();
+    vivo.vibrate();
+    vivo.ring();
+
+// Ques 3
+Square sq = new Square();
+sq.side = 9;
+System.out.println(sq.area()+ "sq");
+System.out.println(sq.perimeter());
+
+//Ques 4
+Rectangle side = new Rectangle();
+side.side1 = 4;
+side.side2 = 6;
+System.out.println(side.area() +"sq");
+System.out.println(side.perimeter());
+
+// Ques 5
+TommyVecetti game = new TommyVecetti();
+game.fire();
+game.hit();
+game.run(); 
+*/
+// Ques6
+
+
+
+}
+    }
+
+

3956535a36a747aa49d19ab9664ab36b5ccc2055
Mohammad-Ayan <ayan2015786@gmail.com>
2024-01-02 00:34:34 +0530

    2-1-24

--- Java+DSA A.C/JavaBasics.java
+++ Java+DSA A.C/JavaBasics.java
@@ -1,17 +1,38 @@
 import java.util.Scanner;
 
-public class JavaBasics{
-    public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-        //String input = sc.next();
-       //System.out.println(input);
+public class JavaBasics {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // String input = sc.next();
+    // System.out.println(input);
 
-       // String input = sc.nextLine();
-       //System.out.println(input);
+    // String input = sc.nextLine();
+    // System.out.println(input);
 
-      // Number input = sc.nextInt();
-      // System.out.println(input);
-    Float price = sc.nextFloat();
-    System.out.println(price);
-    }
+    // Number input = sc.nextInt();
+    // System.out.println(input);
+    // Float price = sc.nextFloat();
+    // System.out.println(price);
+
+    /*
+     * int a = sc.nextInt();
+     * int b = sc.nextInt();
+     * int c = a*b;
+     * System.out.println(c);
+     */
+    // float pie = 3.14;
+    // float radius = sc.nextFloat();
+    // float area = 3.14f * radius * radius;
+    // System.out.println(area);
+    // int a = 10;
+    // int b = a++;
+    // System.out.println(a);
+    // System.out.println(b);
+
+    // ASSIGNMENT OPERATORS
+    // int a = 10 ;
+    // a += 10;
+    // System.out.println(a);
+    
+  }
 }

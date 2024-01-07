c50f98815167adc3464fd860afbca247377fc5a6
Mohammad-Ayan <ayan2015786@gmail.com>
2024-01-08 01:26:43 +0530

    07-01-24

--- Java+DSA A.C/JavaBasics.java
+++ Java+DSA A.C/JavaBasics.java
@@ -457,28 +457,38 @@ public class JavaBasics {
      * }
      * System.out.println();
      * }
+     * 
+     * 
+     * // Q14 Daimond
+     * int n = 4;
+     * for (int i = 1; i <= n; i++) {
+     * for (int j = 1; j <= n - i; j++) {
+     * System.out.print(" ");
+     * }
+     * for (int j = 1; j <= 2 * i-1; j++) {
+     * System.out.print("*");
+     * }
+     * System.out.println();
+     * }
+     * for (int i = n; i >= 1; i--) {
+     * for (int j = 1; j <= n - i; j++) {
+     * System.out.print(" ");
+     * }
+     * for (int j = 1; j <= 2 * i-1; j++) {
+     * System.out.print("*");
+     * }
+     * System.out.println();
+     * }
      
+int n = 5;
+for (int i = n; i>=1; i--){
+  for(int j=1; j<= i; j++){
+  
+    System.out.print(j);
+  }
+  System.out.println();
+}*/
 
-    // Q14 Daimond
-    int n = 4;
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n - i; j++) {
-        System.out.print(" ");
-      }
-      for (int j = 1; j <= 2 * i-1; j++) {
-        System.out.print("*");
-      }
-      System.out.println();
-    }
-    for (int i = n; i >= 1; i--) {
-      for (int j = 1; j <= n - i; j++) {
-        System.out.print(" ");
-      }
-      for (int j = 1; j <= 2 * i-1; j++) {
-        System.out.print("*");
-      }
-      System.out.println();
-    }*/
   }
 }
 

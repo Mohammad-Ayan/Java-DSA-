7678fd9e4362c863c90ee66744722f4403cdfcfd
Mohammad-Ayan <ayan2015786@gmail.com>
2024-01-22 23:26:17 +0530

    21-01-24

--- Java+DSA A.C/Array.java
+++ Java+DSA A.C/Array.java
@@ -72,28 +72,60 @@ public class Array {
     //     System.out.println("The larest No: " + getLargest(numbers));
 
 
-    public static int binarySearch(int numbers[], int key) {
-        int start = 0, end = numbers.length-1; 
+    // public static int binarySearch(int numbers[], int key) {
+    //     int start = 0, end = numbers.length-1; 
 
-        while(start <= end){
-            int mid = (start+end)/2;  
+    //     while(start <= end){
+    //         int mid = (start+end)/2;  
 
-            if (numbers[mid] == key){       //found
-                return mid;
-            }
+    //         if (numbers[mid] == key){       //found
+    //             return mid;
+    //         }
             
-            if (numbers[mid] < key){       //right
-                start = mid + 1;
-            }else{                        //left
-                end = mid - 1;            
-            }
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
-        int numbers[]= {2, 4, 6, 8, 10, 12, 14, 16};
-        int key = 10;
+    //         if (numbers[mid] < key){       //right
+    //             start = mid + 1;
+    //         }else{                        //left
+    //             end = mid - 1;            
+    //         }
+    //     }
+    //     return -1;
+    // }
+    // public static void main(String[] args) {
+    //     int numbers[]= {2, 4, 6, 8, 10, 12, 14, 16};
+    //     int key = 10;
 
-        System.out.println( "The index is: " + binarySearch(numbers, key));
+    //     System.out.println( "The index is: " + binarySearch(numbers, key));
+    
+    // }
+
+    import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        long maxGCD = 0;
+        long maxLCM = (long) N * (N - 1) / 2; // Maximum LCM for consecutive numbers
+
+        for (int i = 2; i <= N; i++) {
+            maxGCD = calculateGCD(maxGCD, i);
+        }
+
+        long difference = maxLCM - maxGCD;
+        System.out.println(difference);
     }
+
+    // Function to calculate GCD (Euclidean Algorithm)
+    private static long calculateGCD(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
+}
+}
+
+

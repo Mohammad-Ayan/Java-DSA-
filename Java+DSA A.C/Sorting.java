74d61a4beb2756d40faab0d5652d54bacba98f7d
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-12 13:52:25 +0530

    .

--- Java+DSA A.C/Sorting.java
+++ Java+DSA A.C/Sorting.java
@@ -1,39 +1,42 @@
+import java.util.Arrays;
 public class Sorting {
-    // ----------------------------------Bubble Sort--------------------------------------------
+    // // -----------------Bubble Sort--------------------
 
     // public static void bubbleSort(int arr[]) {
 
-    // for (int turn = 0; turn < arr.length - 1; turn++) {
-    // for (int j = 0; j < arr.length-1-turn; j++) {
-    // if (arr[j] > arr[j+1]) {
-    // // swap
-    // int temp = arr[j];
-    // arr[j] = arr[j + 1];
-    // arr[j + 1] = temp;
+    //     for (int turn = 0; turn < arr.length - 1; turn++) {
+    //         for (int j = 0; j < arr.length - 1 - turn; j++) { // for turn[1]:- from j = 0 to j = arr.length - 1 - 1 = 2.
+    //                                                           // It compares all elements except the last one: [3, 5, 4]
+    //             if (arr[j] > arr[j + 1]) {
+    //                 // swap
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j + 1];
+    //                 arr[j + 1] = temp;
+
+    //             }
+    //         }
+    //     }
 
     // }
-    // }
-    // }
 
-    // }
     // public static void printArr(int arr[]) {
-    // for (int i = 0; i<arr.length; i++){
-    // System.out.print(arr[i]+ " ");
-    // }
-    // System.out.println();
+    //     for (int i = 0; i < arr.length; i++) {
+    //         System.out.print(arr[i] + " ");
+    //     }
+    //     System.out.println();
     // }
 
     // public static void main(String[] args) {
-    // int arr[] = { 5, 4, 1, 3, 2 };
-    // bubbleSort(arr);
-    // printArr(arr);
+    //     int arr[] = { 5, 4, 1, 3, 2 };
+    //     bubbleSort(arr);
+    //     printArr(arr);
     // }
-    // }
-    // ----------------------------Selection Sort------------------------------------------
+
+ //   // ----------------Selection Sort---------------
 
     // public static void selectionSort(int arr[]) {
     //     for (int i = 0; i < arr.length - 1; i++) {
-    //         int minPos = i;
+    //         int minPos = i;               // curr element = min position
     //         for (int j = i + 1; j < arr.length; j++) {
     //             if (arr[minPos] > arr[j]) {
     //                 minPos = j;
@@ -60,13 +63,85 @@ public class Sorting {
     //     // printArr(arr);
     //     printArr(arr);
     // }
-    //-------------------------------------Insertion Sort ----------------------------------------
+ //   // ------------------Insertion Sort------------------
 
-    public static void insertionSort() {
-        
+//     public static void insertionSort(int arr[]) {
+//         for(int i=1; i<arr.length;i++) {
+//             int curr = arr[i];
+//             int prev = i-1;
+//             //finding out correct pos to insert 
+//             while(prev >= 0 && arr[prev] > curr) {
+//                 arr[prev+1] = arr[prev];
+//                 prev--;
+//             }
+//             //insertion
+//             arr[prev + 1] = curr;
+
+//         }
+//     }
+
+//      // Method to print the array
+//      public static void printArr(int arr[]) {
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+//     int arr[] ={ 5, 4, 1, 3, 2 };
+//     insertionSort(arr);
+//     printArr(arr);
+//     }
+// }
+
+// // ------Inbuilt_Sort[Array.sort(arr)]------
+
+//  public static void printArr(int arr[]) {
+//     for (int i = 0; i < arr.length; i++) {
+//         System.out.print(arr[i] + " ");
+//     }
+//     System.out.println();
+// }
+
+// public static void main(String[] args) {
+//     int arr[] ={ 5, 4, 1, 3, 2 };
+//     Arrays.sort(arr);
+//     printArr(arr);
+// }
+
+ //--------------Counting_Sort------------
+
+ public static void countingSort(int arr[]) {
+    int largest = Integer.MIN_VALUE;
+    for(int i=0; i<arr.length; i++){
+        largest = Math.max(largest, arr[i]);
     }
 
-    public static void main(String[] args) {
-        int arr[] ={ 5, 4, 1, 3, 2 }
+    int count[] = new int[largest + 1];
+    for(int i=0; i<arr.length; i++){  //original Array(iteration)
+        count[arr[i]]++;
     }
+
+    //--sorting
+    int j = 0;
+    for (int i=0; i< count.length; i++){  //frequency Array(iteration)
+        while (count [i] > 0) {
+            arr[j] = i;
+            j++;
+            count[i]--;
+        }
+    }
+}
+ public static void printArr(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+}
+public static void main(String[] args) {
+        int arr[] ={ 5, 4, 1, 3, 2 };
+        countingSort(arr);
+        printArr(arr);
+}
 }
--- Java+DSA A.C/tempCodeRunnerFile.java
+++ Java+DSA A.C/tempCodeRunnerFile.java
@@ -1,9 +1,1 @@
-  public static boolean isPrime(int n) {
-    boolean isPrime = true;
-    for (int i = 2; i <= n - 1; i++) {
-    if (n % i == 0) {  // completely divisible
-    isPrime = false;
-    }
-    }
-    return isPrime;
-    }
+printArr(arr);

4cf628b4bffa71c5e6a6e7492d72d900378cce01
Mohammad-Ayan <ayan2015786@gmail.com>
2024-10-27 09:58:40 +0530

    Updated Matrices_2D.java

--- ayanDsaJava/Matrices_2D.java
+++ ayanDsaJava/Matrices_2D.java
@@ -287,7 +287,7 @@ public class Matrices_2D {
                 transpose[j][i] = matrix[i][j];
             }
         }
-        return trans;
+        return transport;
     }
 
     public static void main(String[] args) {

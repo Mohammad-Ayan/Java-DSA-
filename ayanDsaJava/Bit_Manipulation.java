689c54ee669b118c06a832b8822999937ca6b8c8
Mohammad-Ayan <ayan2015786@gmail.com>
2024-11-14 15:52:22 +0530

    bit

--- ayanDsaJava/Bit_Manipulation.java
+++ ayanDsaJava/Bit_Manipulation.java
@@ -10,7 +10,8 @@ public class Bit_Manipulation {
 
     // }
 
-    // ---------------------------------------Odd_Or_Even=--------------------------------------------
+    // -----------------Odd_Or_Even(bitMask)---------------
+
     // public static void OddOrEven(int n) {
     // int bitMask = 1;
     // if ((n & bitmask) == 0) {
@@ -25,15 +26,41 @@ public class Bit_Manipulation {
     // OddOrEven(9);
     // OddOrEven(2);
     // }
-    public static int getIthBit(int n, int i) {
-        int bitMask = 1 << i;
-        if ((n & bitMask) == 0) {
-            return 0;
-        } else {
-            return 1;
-        }
+
+    // ----------------Get_ith_bit -----------------
+
+    // public static int getIthBit(int n, int i) {
+    // int bitMask = 1 << i;
+    // if ((n & bitMask) == 0) {
+    // return 0;
+    // } else {
+    // return 1;
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // System.out.println(getIthBit(10, 2)); // 1010
+    // }
+
+    // -------------------Set ith bit----------------
+
+    // public static int setIthBit(int n, int i) {
+    //     int bitMask = 1 << i;
+    //     return n | bitMask;
+    // }
+
+    // public static void main(String[] args) {
+    //     System.out.println(setIthBit(10, 2)); // 1010
+    // }
+
+    // -------------------Clear ith bit----------------
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
     }
+
     public static void main(String[] args) {
-        System.out.println(getIthBit(10, 2));
+        System.out.println(clearIthBit(10, 2)); // 1010
     }
+
 }

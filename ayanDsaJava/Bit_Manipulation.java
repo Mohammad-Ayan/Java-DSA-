2c6a348993ac8c03a34d4a67aed1c8c4f0f6e3a8
Mohammad-Ayan <ayan2015786@gmail.com>
2025-01-07 12:12:52 +0530

    Getters & Setters

--- ayanDsaJava/Bit_Manipulation.java
+++ ayanDsaJava/Bit_Manipulation.java
@@ -39,28 +39,42 @@ public class Bit_Manipulation {
     // }
 
     // public static void main(String[] args) {
-    // System.out.println(getIthBit(10, 2)); // 1010
+    // System.out.println(getIthBit(10, 2)); // 1010 --> 0/1
     // }
 
     // -------------------Set ith bit----------------
 
-    // public static int setIthBit(int n, int i) {
-    //     int bitMask = 1 << i;
-    //     return n | bitMask;
-    // }
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
 
     // public static void main(String[] args) {
-    //     System.out.println(setIthBit(10, 2)); // 1010
+    //     System.out.println(setIthBit(10, 2)); // 1010 --> 1000
     // }
 
     // -------------------Clear ith bit----------------
+
     public static int clearIthBit(int n, int i) {
         int bitMask = ~(1 << i);
         return n & bitMask;
     }
 
-    public static void main(String[] args) {
-        System.out.println(clearIthBit(10, 2)); // 1010
+    // public static void main(String[] args) {
+    //     System.out.println(clearIthBit(10, 1)); // 1010 -->  1000
+    // }
+ 
+    // -------------------Update ith bit----------------
+
+    public static int updateIthBit(int n, int i, int newBit) {
+        if(newBit == 0) {
+            return clearIthBit(n, i);
+        }else{
+            return setIthBit(n, i);
+        }
     }
 
+    public static void main(String[] args) {
+        System.out.println(updateIthBit(10, 2, 1));
+    }
 }
---
+++ ayanDsaJava/Oops.java
@@ -0,0 +1,80 @@
+public class Oops {
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); // created a pen object p1.
+        p1.setColor("Yellow");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "Ayan";
+        // myAcc.password = "pqr" //Can't be accessed As it is Private
+        myAcc.setPassword("abcd"); // It will just change, Can't be accessed
+    }
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPassword(String pwd) {
+        password = pwd;
+    }
+
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calPercentage(int Phy, int Maths, int Chem) {
+        percentage = (Phy + Maths + Chem / 3);
+    }
+
+    // -----------Getters and Setters------------
+
+    public static void main(String[] args) {
+        Pen1 p2 = new Pen1(); // created a pen object p1.
+        p2.setColor("Yellow");
+        System.out.println(p2.getColor());
+        p2.setTip(5);
+        System.out.println(p2.getTip());
+    }
+}
+
+class Pen1 {
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+//--------------

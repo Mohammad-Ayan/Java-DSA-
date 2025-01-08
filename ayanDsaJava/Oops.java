0ae6f144ab812c800654928d4561ad205c6172a8
Mohammad-Ayan <ayan2015786@gmail.com>
2025-01-08 09:36:53 +0530

    Function Overriding

--- ayanDsaJava/Oops.java
+++ ayanDsaJava/Oops.java
@@ -146,22 +146,42 @@ public class Oops {
 
 //-------------Polymorphism--------------
 //--Function Overloading--
+//     public static void main(String[] args) {
+//         Calculator calci = new Calculator();
+//         System.out.println(calci.sum(1,2));
+//         System.out.println(calci.sum((float)1.5,(float)2.4));
+//         System.out.println(calci.sum(1,2, 2));
+//     }
+// //}
+
+//     class Calculator{
+//         int sum(int a,  int b){
+//             return a + b;
+//         }
+//         float sum(float a,  float b){
+//             return a + b;
+//         }
+//         int sum(int a,  int b, int c){
+//             return a + b + c;
+//         }
+//     }
+
+//--Function Overriding--
+
     public static void main(String[] args) {
-        Calculator calci = new Calculator();
-        System.out.println(calci.sum(1,2));
-        System.out.println(calci.sum((float)1.5,(float)2.4));
-        System.out.println(calci.sum(1,2, 2));
+        Deer bn = new Deer();
+        bn.eat();
     }
 }
 
-    class Calculator{
-        int sum(int a,  int b){
-            return a + b;
-        }
-        float sum(float a,  float b){
-            return a + b;
-        }
-        int sum(int a,  int b, int c){
-            return a + b + c;
+    class Animal{
+        void eat(){
+            System.out.println("eats anything");
         }
     }
+    class Deer extends Animal{
+        void eat(){
+            System.out.println("eats grass");
+        }
+    }
+    

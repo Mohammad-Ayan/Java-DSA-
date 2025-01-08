ed889e8eb8b8034ea16132a1383a33ac902faa83
Mohammad-Ayan <ayan2015786@gmail.com>
2025-01-08 09:13:49 +0530

    Inheritance

--- ayanDsaJava/Oops.java
+++ ayanDsaJava/Oops.java
@@ -1,105 +1,149 @@
 public class Oops {
+//     public static void main(String[] args) {
+//         Pen p1 = new Pen(); // created a pen object p1.
+//         p1.setColor("Yellow");
+//         System.out.println(p1.color);
+//         p1.setTip(5);
+//         System.out.println(p1.tip);
+
+//         BankAccount myAcc = new BankAccount();
+//         myAcc.username = "Ayan";
+//         // myAcc.password = "pqr" //Can't be accessed As it is Private
+//         myAcc.setPassword("abcd"); // It will just change, Can't be accessed
+//     }
+// }
+
+// class BankAccount {
+//     public String username;
+//     private String password;
+
+//     public void setPassword(String pwd) {
+//         password = pwd;
+//     }
+
+// }
+
+// class Pen {
+//     String color;
+//     int tip;
+
+//     void setColor(String newColor) {
+//         color = newColor;
+//     }
+
+//     void setTip(int newTip) {
+//         tip = newTip;
+//     }
+// }
+
+// class Student {
+//     String name;
+//     int age;
+//     float percentage;
+
+//     void calPercentage(int Phy, int Maths, int Chem) {
+//         percentage = (Phy + Maths + Chem / 3);
+//     }
+
+//     // -----------Getters and Setters------------
+
+//     public static void main(String[] args) {
+//         Pen1 p2 = new Pen1(); // created a pen object p1.
+//         p2.setColor("Yellow");
+//         System.out.println(p2.getColor());
+//         p2.setTip(5);
+//         System.out.println(p2.getTip());
+//     }
+// }
+
+// class Pen1 {
+//     private String color;
+//     private int tip;
+
+//     String getColor() {
+//         return this.color;
+//     }
+
+//     int getTip() {
+//         return this.tip;
+//     }
+
+//     void setColor(String newColor) {
+//         color = newColor;
+//     }
+
+//     void setTip(int newTip) {
+//         tip = newTip;
+//     }
+
+//     // --------------Constructor------------
+
+//     public static void main(String[] args) {
+//         Students s3 = new Students();
+//         Students s4 = new Students("Ayan");
+//         Students s5 = new Students(1234);
+
+//     }
+// }
+
+// class Students {
+//     String name;
+//     int roll;
+
+//     Students() {
+//         System.out.println("Constructor is called...");
+//     }
+
+//     Students(String name) {
+//         this.name = name;
+//     }
+
+//     Students(int roll) {
+//         this.roll = roll;
+//     }
+
+    // -----------Inheritance---------------
+
     public static void main(String[] args) {
-        Pen p1 = new Pen(); // created a pen object p1.
-        p1.setColor("Yellow");
-        System.out.println(p1.color);
-        p1.setTip(5);
-        System.out.println(p1.tip);
-
-        BankAccount myAcc = new BankAccount();
-        myAcc.username = "Ayan";
-        // myAcc.password = "pqr" //Can't be accessed As it is Private
-        myAcc.setPassword("abcd"); // It will just change, Can't be accessed
+        Dog tom = new Dog();
+        tom.legs = 4;
+        tom.eat();
+        System.out.println(tom.legs);
     }
 }
 
-class BankAccount {
-    public String username;
-    private String password;
+    // Base Class
+    class Animal {
+        String color;
 
-    public void setPassword(String pwd) {
-        password = pwd;
+        void eat() {
+            System.out.println("eats");
+        }
+
+        void breathe() {
+            System.out.println("breathes");
+        }
     }
 
-}
+    //Derived Class
+    // class Fish extends Animal{
+    //     int fins;
 
-class Pen {
-    String color;
-    int tip;
+    //     void swim(){
+    //         System.out.println("Swims in water");
+    //     }
+    //}
 
-    void setColor(String newColor) {
-        color = newColor;
+    //Derived Class
+    class Mammal extends Animal{
+        int legs;
     }
 
-    void setTip(int newTip) {
-        tip = newTip;
-    }
-}
-
-class Student {
-    String name;
-    int age;
-    float percentage;
-
-    void calPercentage(int Phy, int Maths, int Chem) {
-        percentage = (Phy + Maths + Chem / 3);
+    //Derived Class
+    class Dog extends Mammal{
+        String breed;
     }
 
-    // -----------Getters and Setters------------
+//-------Poly
 
-    public static void main(String[] args) {
-        Pen1 p2 = new Pen1(); // created a pen object p1.
-        p2.setColor("Yellow");
-        System.out.println(p2.getColor());
-        p2.setTip(5);
-        System.out.println(p2.getTip());
-    }
-}
-
-class Pen1 {
-    private String color;
-    private int tip;
-
-    String getColor() {
-        return this.color;
-    }
-
-    int getTip() {
-        return this.tip;
-    }
-
-    void setColor(String newColor) {
-        color = newColor;
-    }
-
-    void setTip(int newTip) {
-        tip = newTip;
-    }
-
-    // --------------Constructor------------
-
-    public static void main(String[] args) {
-        Students s3 = new Students();
-        Students s4 = new Students("Ayan");
-        Students s5 = new Students(1234);
-      
-
-    }
-}
-
-class Students {
-    String name;
-    int roll;
-
-    Students() {
-        System.out.println("Constructor is called...");
-    }
-
-    Students(String name) {
-        this.name = name;
-    }
-
-    Students(int roll) {
-        this.roll = roll;
-    }
 }

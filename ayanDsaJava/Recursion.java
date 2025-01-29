98d497042b931d9d451936d20162af3dc6049180
Mohammad-Ayan <ayan2015786@gmail.com>
2025-01-29 09:53:01 +0530

    started recursion

--- ayanDsaJava/Recursion.java
+++ ayanDsaJava/Recursion.java
@@ -7,11 +7,21 @@ public class Recursion {
 
         System.out.print(n + " ");
         printDec(n - 1);
+    }
 
+    public static void printInc(int n) {
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        printDec(n - 1);
+        System.out.print(n + " ");
     }
 
     public static void main(String[] args) {
         int n = 10;
         printDec(n);
+        // printInc(n);
+        // printFact(n);
     }
 }

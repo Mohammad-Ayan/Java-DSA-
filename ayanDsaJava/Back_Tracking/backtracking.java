d8f93060940c50b4882700db463f04a61e167959
Mohammad-Ayan <ayan2015786@gmail.com>
2025-02-26 23:39:23 +0530

    N-Queen Problem

--- ayanDsaJava/Back_Tracking/backtracking.java
+++ ayanDsaJava/Back_Tracking/backtracking.java
@@ -60,13 +60,76 @@ public class backtracking {
         }
     }
 
+    // -------------N-Queens Problem---------------
+
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertically up
+        for(int i = row; i>=0; i--){
+            if(board[i] [col] == 'Q'){
+                return false;
+            }
+        }
+
+        // daigonal left up
+        for (int i = row-1, j = col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // daigonal right
+        for(int i= row-1, j= col+1; i>=0 && j < board.length; i++, j++){
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }  
+    public static void nQueens(char board[][], int row) {
+        // base case
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+
+        // column
+        for (int j = 0; j < board.length; j++) {
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';
+                nQueens(board, row + 1); // function call
+                board[row][j] = '.'; // backtracking step
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("----chess_Board----");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String[] args) {
         // int arr[] = new int[5];
         // changeArr(arr, 0, 1);
         // printArr(arr);
 
-        String str = "abc";
+        // String str = "abc";
         // findSubsets(str, "", 0);
-        findPermutation(str, "");
+        // findPermutation(str, "");
+
+        int n = 4;
+        char board[][] = new char[n][n];
+        // initialize
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        nQueens(board, 0);
     }
 }

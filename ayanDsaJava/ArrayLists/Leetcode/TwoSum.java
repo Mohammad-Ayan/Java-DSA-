c4e23082b36e64580af0af69d4f7f530337992f9
Mohammad Ayan <ayan2015786@gmail.com>
2025-06-22 12:43:45 +0530

    two_sum

---
+++ ayanDsaJava/ArrayLists/Leetcode/TwoSum.java
@@ -0,0 +1,21 @@
+package ArrayLists.Leetcode;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int required = target - nums[i]; // the number we need
+
+            if (map.containsKey(required)) {
+                return new int[] { map.get(required), i };
+            }
+
+            map.put(nums[i], i); // store current number with its index
+        }
+
+        return new int[] {}; // if no pair found
+    }
+}

1685bb6de00c8e6fe26da635ad6e147dd403f52d
Mohammad Ayan <ayan2015786@gmail.com>
2025-07-31 11:55:10 +0530

    Detect a Cycle in a LL

--- ayanDsaJava/LinkedList/LinkedList.java
+++ ayanDsaJava/LinkedList/LinkedList.java
@@ -250,6 +250,21 @@ public class LinkedList {
         return true;
     }
 
+    // --------Detect a Loop/Cycle in a LL-------
+    public static boolean isCycle(){ // Floyd's CFA
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+            if(fast == slow){
+                return true;  // cycle exist
+            }
+        }
+         return false; // cycle not exist (linear LL)
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         // ll.addFirst(2);
@@ -280,9 +295,17 @@ public class LinkedList {
         ll.addLast(2);
         ll.addLast(2);
         ll.addLast(1);
-        // ll.addLast(1);
+        ll.addLast(1);
 
         ll.print();
         System.out.println(ll.checkPalindrome());
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head;
+        // 1->2->3->4->1
+        System.out.println(isCycle());
     }
 }
